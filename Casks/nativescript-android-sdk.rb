fb6594985850910d26df0ebbeeaabaf5f19738a5
NickBranstein <nick@brosteins.com>
2017-05-14 23:03:26 -0400

    updates

--- Casks/nativescript-android-sdk.rb
+++ Casks/nativescript-android-sdk.rb
@@ -8,22 +8,11 @@ cask 'nativescript-android-sdk' do
   homepage 'https://developer.android.com/index.html'
 
   build_tools_version = '22.0.1'
-  path = 'android-sdk'
-
-  binary "tools/android"
-
-  postflight do
-    FileUtils.ln_sf(path.to_s, "#{HOMEBREW_PREFIX}/Caskroom/android-sdk")
-  end
-
-  uninstall_postflight do
-    FileUtils.rm("#{HOMEBREW_PREFIX}/Caskroom/android-sdk")
-  end
 
   caveats <<-EOS.undent
     Now run android update sdk to get the old GUI and update the android SDKs.
     You may need to fix your profile with the following update:
-      'export ANDROID_SDK_ROOT=#{HOMEBREW_PREFIX}/Caskroom/android-sdk'
+      'export ANDROID_SDK_ROOT=#{HOMEBREW_PREFIX}/Caskroom/nativescript-android-sdk'
     This operation may take up to 10 minutes depending on your internet connection.
     Please, be patient.
   EOS
