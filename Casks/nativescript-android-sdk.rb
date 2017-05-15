8e6a700f912585f53380e00deddfe9d47c40801e
NickBranstein <nick@brosteins.com>
2017-05-15 00:17:11 -0400

    final version

--- Casks/nativescript-android-sdk.rb
+++ Casks/nativescript-android-sdk.rb
@@ -1,15 +1,16 @@
 cask 'nativescript-android-sdk' do
+  version '22.0.1'
   sha256 '0a3fcda0eb90195f1d9926ea756307d38f6801e10289afe14bd15d0c155cd1ee'
 
   # google.com/android/repository/tools_r was verified as official when first introduced to the cask
-  url "https://dl.google.com/android/repository/tools_r22.0.1-macosx.zip"
+  url "https://dl.google.com/android/repository/tools_r#{version}-macosx.zip"
   name 'NativeScript - Android SDK'
   homepage 'https://developer.android.com/index.html'
 
   caveats <<-EOS.undent
     Now run android update sdk to get the old GUI and update the android SDKs.
     You may need to fix your profile with the following update:
-      'export ANDROID_SDK_ROOT=#{HOMEBREW_PREFIX}/Caskroom/nativescript-android-sdk/'
+      'export ANDROID_SDK_ROOT=#{HOMEBREW_PREFIX}/Caskroom/nativescript-android-sdk/22.0.1'
     This operation may take up to 10 minutes depending on your internet connection.
     Please, be patient.
   EOS
