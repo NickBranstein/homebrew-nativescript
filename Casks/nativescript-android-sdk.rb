bf361961f29eb8d7e3a008bb3c31d3ea73ef80e9
NickBranstein <nick@brosteins.com>
2017-05-14 22:34:50 -0400

    path

--- Casks/nativescript-android-sdk.rb
+++ Casks/nativescript-android-sdk.rb
@@ -13,7 +13,7 @@ cask 'nativescript-android-sdk' do
   binary "#{path}/tools/android"
 
   preflight do
-    system_command "#{staging_path}/tools/android", args: ['update sdk --filter tools,platform-tools,android-25,build-tools-25.0.2,extra-android-m2repository,extra-google-m2repository,extra-android-support --all --no-ui'], input: 'y'
+    system_command "#{staged_path}/tools/android", args: ['update sdk --filter tools,platform-tools,android-25,build-tools-25.0.2,extra-android-m2repository,extra-google-m2repository,extra-android-support --all --no-ui'], input: 'y'
   end
 
   postflight do
